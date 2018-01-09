c91f57d657a8b1f8ca6c32928d250938f4286913
Ellen Cornelius <ecorneli@iwu.edu>
2018-01-09 09:28:18 -0700

    Add pantry methods and tests for iteration 1

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,21 @@
 class Pantry
 
+  attr_reader :stock
+
+  def initialize
+    @stock = {}
+  end
+
+  def stock_check(ingredient)
+    @stock[ingredient]
+    if @stock[ingredient] == nil
+      return 0
+    end
+  end
+
+  def restock(ingredient, quantity)
+    require 'pry'; binding.pry
+    @stock[ingredient] += quantity
+  end
+
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,27 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
 
+  def test_stocks_starts_as_empty
+    pantry = Pantry.new
+
+    assert_equal Hash.new, pantry.stock
+  end
+
+  def test_stock_test_returns_ingredient_in_pantry
+    skip
+    pantry = Pantry.new
+
+    assert_equal 0, pantry.stock_check("Cheese")
+  end
+
+  def test_can_restock_ingredients_in_pantry
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+
+    assert_equal 10, pantry.stock_check("Cheese")
+
+    pantry.restock("Cheese", 20)
+    assert_equal 30, pantry.stock_check("Cheese")
+  end
+
 end
